7c6f1694ea935cf209613e37540ef20483175f3e
hadim8455 <hadim8455@gmail.com>
2025-05-18 14:19:27 +0500

    Complete Login/SignUp & TodoApp

--- profile.js
+++ profile.js
@@ -1,6 +1,5 @@
   function getUserLogin(){
     var profile = JSON.parse(localStorage.getItem("userLogin"))
-    // console.log(profile)
 
     var firstName = document.getElementById("firstName")
     var lastName = document.getElementById("lastName")
@@ -47,52 +46,3 @@ function updateUser(){
 
 }
 
-
-// sir code
-
-/**
-function getUserLogin() {
-    var profile = JSON.parse(localStorage.getItem("userLogin"))
-    // console.log("getUserData", userData)
-
-    var firstName = document.getElementById("firstName")
-    var lastName = document.getElementById("lastName")
-    var email = document.getElementById("email")
-    var password = document.getElementById("password")
-
-    firstName.value = profile.firstName
-    lastName.value = profile.lastName
-    email.value = profile.email
-    password.value = profile.password
-
-
-}
-
-
-function updateUser() {
-    var firstName = document.getElementById("firstName")
-    var lastName = document.getElementById("lastName")
-    var email = document.getElementById("email")
-    var password = document.getElementById("password")
-
-    var profileObject = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        password: password.value,
-    }
-
-    var userArr = JSON.parse(localStorage.getItem("user"))
-    var indexNumber;
-    for (var i = 0; i < userArr.length; i++) {
-        if (userArr[i].email === email.value) {
-            // console.log("indexNumber", i)
-            indexNumber = i
-        }
-    }
-
-    userArr[indexNumber] = profileObject
-    console.log("userArr", userArr)
-    localStorage.setItem("user", JSON.stringify(userArr))
-
-}   **/

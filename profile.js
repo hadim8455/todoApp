e7b1fa7e2964896c7322049764262289540577de
hadim8455 <hadim8455@gmail.com>
2025-05-18 14:18:19 +0500

    Complete Login/SignUp & TodoApp

--- profile.js
+++ profile.js
@@ -1,35 +1,38 @@
-function getUserLogin(){
+  function getUserLogin(){
     var profile = JSON.parse(localStorage.getItem("userLogin"))
     // console.log(profile)
 
     var firstName = document.getElementById("firstName")
-    var secondName = document.getElementById("secondName")
+    var lastName = document.getElementById("lastName")
     var email = document.getElementById("email")
     var password = document.getElementById("password")
 
     
     firstName.value = profile.firstName
-    secondName.value = profile.secondName
+    lastName.value = profile.lastName
     email.value = profile.email
     password.value = profile.password
 
     
 }
-return
 function updateUser(){
     var firstName = document.getElementById("firstName")
-    var secondName = document.getElementById("secondName")
+    var lastName = document.getElementById("lastName")
     var email = document.getElementById("email")
     var password = document.getElementById("password")
-    var updateObject = {
+
+    var profileObject = {
         firstName: firstName.value,
-        secondName: secondName.value,
+        lastName: lastName.value,
         email: email.value,
         password: password.value,
     }
-return
+
+    
     var userArr = JSON.parse(localStorage.getItem("user"))
-    // console.log(userArr)
+    console.log("arry",userArr[1])
+
+
 
     var indexNumber;
 
@@ -39,7 +42,57 @@ return
             console.log("userName", indexNumber)
         }
     }
-    userArr[indexNumber] = updateObject
+    userArr[indexNumber] = profileObject
     localStorage.setItem("user", JSON.stringify(userArr))
 
 }
+
+
+// sir code
+
+/**
+function getUserLogin() {
+    var profile = JSON.parse(localStorage.getItem("userLogin"))
+    // console.log("getUserData", userData)
+
+    var firstName = document.getElementById("firstName")
+    var lastName = document.getElementById("lastName")
+    var email = document.getElementById("email")
+    var password = document.getElementById("password")
+
+    firstName.value = profile.firstName
+    lastName.value = profile.lastName
+    email.value = profile.email
+    password.value = profile.password
+
+
+}
+
+
+function updateUser() {
+    var firstName = document.getElementById("firstName")
+    var lastName = document.getElementById("lastName")
+    var email = document.getElementById("email")
+    var password = document.getElementById("password")
+
+    var profileObject = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        password: password.value,
+    }
+
+    var userArr = JSON.parse(localStorage.getItem("user"))
+    var indexNumber;
+    for (var i = 0; i < userArr.length; i++) {
+        if (userArr[i].email === email.value) {
+            // console.log("indexNumber", i)
+            indexNumber = i
+        }
+    }
+
+    userArr[indexNumber] = profileObject
+    console.log("userArr", userArr)
+    localStorage.setItem("user", JSON.stringify(userArr))
+
+}   **/
